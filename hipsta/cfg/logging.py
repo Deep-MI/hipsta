754d961494c0c3ba3b19d5824cc84d2d3bc3fa04
diersk <kersten.diers@dzne.de>
2023-08-22 19:07:33 +0200

    Check temporary logfiles

--- hipsta/cfg/logging.py
+++ hipsta/cfg/logging.py
@@ -5,6 +5,7 @@
 import logging
 import os
 import sys
+import tempfile
 import time
 
 from .version import get_version
@@ -22,7 +23,28 @@ def setup_logging(args):
     """
     Set up logging
     """
-    
+
+    # check if output directory exists or can be created
+    if os.path.isdir(args.outputdir):
+        logging.info("Found output directory " + args.outputdir)
+    else:
+        try:
+            os.mkdir(args.outputdir)
+        except OSError as e:
+            logging.error("Cannot create output directory " + args.outputdir)
+            logging.error("Reason: " + str(e))
+            raise
+
+    # check if logfile can be written in output directory
+    try:
+        testfile = tempfile.TemporaryFile(dir=args.outputdir)
+        testfile.close()
+    except OSError as e:
+        logging.error(args.outputdir + " not writeable")
+        logging.error("Reason: " + str(e))
+        raise
+
+    # setup logging
     logfile =  os.path.join(args.outputdir, 'logfile.txt')
     logfile_format = "[%(levelname)s: %(filename)s] %(message)s"
     logfile_handlers_stream = logging.StreamHandler(sys.stdout)
