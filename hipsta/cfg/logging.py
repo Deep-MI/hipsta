ba3e5ceaf665b474c78d76567a26571c72c6fa22
diersk <kersten.diers@dzne.de>
2023-12-10 16:21:28 +0100

    Fixed logging

--- hipsta/cfg/logging.py
+++ hipsta/cfg/logging.py
@@ -26,13 +26,13 @@ def setup_logging(args):
 
     # check if output directory exists or can be created
     if os.path.isdir(args.outputdir):
-        logging.info("Found output directory " + args.outputdir)
+        print("Found output directory " + args.outputdir)
     else:
         try:
             os.mkdir(args.outputdir)
         except OSError as e:
-            logging.error("Cannot create output directory " + args.outputdir)
-            logging.error("Reason: " + str(e))
+            print("Cannot create output directory " + args.outputdir)
+            print("Reason: " + str(e))
             raise
 
     # check if logfile can be written in output directory
@@ -40,8 +40,8 @@ def setup_logging(args):
         testfile = tempfile.TemporaryFile(dir=args.outputdir)
         testfile.close()
     except OSError as e:
-        logging.error(args.outputdir + " not writeable")
-        logging.error("Reason: " + str(e))
+        print(args.outputdir + " not writeable")
+        print("Reason: " + str(e))
         raise
 
     # setup logging
@@ -56,7 +56,8 @@ def setup_logging(args):
 
     logging.basicConfig(level=logging.INFO, format=logfile_format, handlers=[logfile_handlers_file, logfile_handlers_stream])
 
-    # intial messages
+    # intial messages (do not use logging earlier, since this will do the
+    # default setup, and subsequent custom setup will have no effects)
     LOGGER.info("Starting logging ...")
     LOGGER.info("Logfile: %s", logfile)
     LOGGER.info("Version: %s", get_version())
@@ -64,29 +65,3 @@ def setup_logging(args):
 
     # log args
     LOGGER.info("Command: " + " ".join(sys.argv))
-    
-
-#    logfile =  os.path.join(args.outputdir, 'logfile.txt')
-
-#     # check if output directory exists or can be created
-#     if os.path.isdir(args.outputdir):
-#         logging.info("Found output directory " + args.outputdir)
-#     else:
-#         try:
-#             os.mkdir(args.outputdir)
-#         except OSError as e:
-#             logging.error("Cannot create output directory " + args.outputdir)
-#             logging.error("Reason: " + str(e))
-#             raise
-
-#     # check if logfile can be written in output directory
-#     try:
-#         testfile = tempfile.TemporaryFile(dir=args.outputdir)
-#         testfile.close()
-#     except OSError as e:
-#         logging.error(args.outputdir + " not writeable")
-#         logging.error("Reason: " + str(e))
-#         raise
-
-#     # return
-#     return args
